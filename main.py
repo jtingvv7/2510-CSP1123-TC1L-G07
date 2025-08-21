f858f0f637cb6d0b08e86a091fabab39bfcf4ae2
Joan Wong <huangjiating17@gmail.com>
2025-08-21 14:40:37 +0800

    joan folder

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Halo")
+from flask import Flask
+from Joan.transaction.routes import transaction_bp
+
+
+app = Flask(__name__)
+
+# register blueprint
+app.register_blueprint(transaction_bp, url_prefix="/transaction")
+
+
+if __name__ == "_main_":
+    app.run(debug=True)
