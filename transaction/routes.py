be09e24624f65e819cf630b920577391c202d248
SEE JIA WEN <vivoy81.wyaness@gmail.com>
2025-08-23 00:49:57 +0800

    Add something

--- transaction/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-transaction_bp = Blueprint('transaction', __name__, template_folder='templates', static_folder='static')
-
-@transaction_bp.route("/")
-def index():
-    return render_template("transaction/transaction.html")
