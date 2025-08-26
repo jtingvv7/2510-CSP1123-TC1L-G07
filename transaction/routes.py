8e4846238186be348461ffc32bfa8f84a90b99a9
Joan Wong <huangjiating17@gmail.com>
2025-08-26 15:17:00 +0800

    add check record &  cancel transaction

--- transaction/routes.py
+++ transaction/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for , flash
 from flask_login import  login_required , current_user
 from datetime import datetime, timezone
 from models import db, Product, Transaction
+from sqlalchemy.exc import SQLAlchemyError
 
 
 transaction_bp = Blueprint('transaction', __name__, template_folder='templates', static_folder='static')
@@ -20,21 +21,59 @@ def buy_product(product_id):
         return redirect (url_for("transaction.home"))
     
     #prevent duplicate purchase requests
-    existing = Transaction.query.filter_by( buyer_id = current_user,
+    existing = Transaction.query.filter_by( buyer_id = current_user.id,
                                            product_id = product_id,
-                                           status = "pending").first()
+                                           status = "pending").first() 
     if existing:
         flash("You have already requested to purchase this product","info")
         return redirect(url_for("transaction.home"))
 
-    new_transaction = Transaction (buyer_id = current_user.id,
+    try:
+        new_transaction = Transaction (buyer_id = current_user.id,
                                    product_id = product_id,
-                                   status = "pending")
-    db.session.add(new_transaction)
-    db.session.commit()
+                                   status = Transaction.status_pending)  #use a constant 
+        db.session.add(new_transaction)
+        db.session.commit()
+        flash("Purchase request send! Waiting for seller confirmation","success")
+    except SQLAlchemyError: 
+        db.session.rollback()
+        flash("An error occurred while processing your request.","danger")
 
-    flash("Purchase request send! Waiting for seller confirmation","success")
-    return redirect(url_for("transaction.index"))
+
+        return redirect(url_for("transaction.index"))
+
+#check transaction record & check request     
+@transaction_bp.route("/my_transactions") 
+@login_required
+def my_transaction():
+    transactions = Transaction.query.filter_by(buyer_id = current_user.id).all  #check all owner by current user transaction record
+    return render_template("transaction/my_transactions.html", transactions = transactions )
+
+#buyer want to cancel transaction when pending state
+@transaction_bp.route("cancel/ <int: trancaction_id >",methods = ["POST"])
+@login_required
+def cancel_transaction(transaction_id): #user cannot delete transaction for others
+    transaction = Transaction.query.get_or_404(transaction_id)
+    if transaction.buyer_id != current_user.id :
+        flash("You cannot cancel this transaction.","warning")
+        return redirect(url_for("transaction.my_transactions"))
+    
+    if transaction.status != "pending": #only transaction in pending state can be cancelled
+        flash("Only pending requests can be cancelled,","warning")
+        return redirect(url_for("transaction.my_transactions"))
+    
+    try:
+        transaction.status = "cancelled"
+        db.session.commit()
+        flash("Transaction cancelled successsfully.","success")
+    except SQLAlchemyError:
+        db.session.rollback()
+        flash("Error cancelling transaction.","danger")
+
+    return redirect(url_for("transaction.my_transactions"))
+
+
+
 
 #transaction front page
 @transaction_bp.route("/")
