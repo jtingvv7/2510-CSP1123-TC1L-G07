febf4a7cd6991e7f247e1a689f9976a434e17816
cy <inl56779@gmail.com>
2025-08-26 21:40:15 +0800

    update profile

---
+++ usersystem/main.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///secondloop.db"  #connect database
+app.config["SQLALCHEMY_TRACH_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)  
+
+from models import User  # import models AFTER db is created
+
+with app.app_context():
+    db.create_all()  # make sure tables are created
+    
+from transaction.routes import transaction_bp
+# register blueprint
+app.register_blueprint(transaction_bp, url_prefix="/transaction")
+
+
+if __name__ == "_main_":
+    app.run(debug=True) 
