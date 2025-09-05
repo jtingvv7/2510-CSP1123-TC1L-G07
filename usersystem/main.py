5b93849d58932ed73601a4ecf26130d028ba362b
SEE JIA WEN <vivoy81.wyaness@gmail.com>
2025-09-06 01:22:32 +0800

    recovery the folder

--- usersystem/main.py
+++ usersystem/main.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "supersecretkey"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///secondloop.db"  #connect database
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -13,10 +14,5 @@ import models  # import models AFTER db is created
 with app.app_context():
     db.create_all()  # make sure tables are created
 
-from transaction.routes import transaction_bp
-# register blueprint
-app.register_blueprint(transaction_bp, url_prefix="/transaction")
-
-
-if __name__ == "_main_":
-    app.run(debug=True) 
+if __name__ == "__main__":
+    app.run(debug=True)
